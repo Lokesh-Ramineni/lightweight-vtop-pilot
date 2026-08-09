086becd27f9622a5b077c66a60f7c34cbf45a91f
Lokesh-Ramineni <lokesh.ramineni1@gmail.com>
2026-08-09 12:17:57 +0530

    updated to add logs

--- services/TIMETABLE/service.py
+++ services/TIMETABLE/service.py
@@ -1,21 +1,30 @@
-from http import cookies
+# from http import cookies
+import httpx
+import logging
+from pathlib import Path
+from logging_config import setup_logging
 
 from enpoints import TIME_TABLE_PRE,PROCESS_TABLE
-from pathlib import Path
 from services.TIMETABLE.parser import fetching_timetable
+
+
+
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 timetable_path = ROOT_DIR / "data" / "cache"
-import httpx
+
+logger = logging.getLogger(__name__)
+
 async def time_table(client,vtop_engine,data1,data2):
     ck = httpx.Cookies()
     ck.set("JSESSIONID", vtop_engine)
 
-    r=await client.post(TIME_TABLE_PRE,cookies=ck,data=data1)
-    # print(client.cookies)
-    # print(r.status_code)
-    s=await client.post(PROCESS_TABLE,data=data2,cookies=ck)
-    # print(s.url)
-    # print(s.status_code)
-    fetching_timetable(s.text)
+    # r=await client.post(TIME_TABLE_PRE,cookies=ck,data=data1)
+    logger.info("Requesting timetable")
+    try:
+        s=await client.post(PROCESS_TABLE,data=data2,cookies=ck)
+        logger.info("Requesting success")
+        fetching_timetable(s.text)
+    except Exception as e:
+        logger.error("Requesting timetable failed",e)
     # with open(timetable_path/"timetable.html","wb") as f:
     #     f.write(s.content)
