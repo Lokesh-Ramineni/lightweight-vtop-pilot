ea26eedbe8c44a5e6d0bc020b7fbc95ec4a909a2
Lokesh-Ramineni <lokesh.ramineni1@gmail.com>
2026-09-06 18:43:54 +0530

    bug fix

--- src/login_check.py
+++ src/login_check.py
@@ -1,9 +1,5 @@
 from bs4 import BeautifulSoup
 def login_ch(html_src):
     soup = BeautifulSoup(html_src, 'lxml')
-    check=soup.find("h4[class='fw-bold']")
-
-    if check == "VTOP Login":
-        return True
-    else:
-        return False
+    check = soup.find("h4", class_="fw-bold")
+    return check is not None and "VTOP Login" in check.get_text()
