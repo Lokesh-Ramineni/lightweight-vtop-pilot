0eaaf718de8830a49e0348cf59102982161fe42e
Lokesh-Ramineni <lokesh.ramineni1@gmail.com>
2026-09-08 13:45:23 +0530

    error handling and logging

--- services/GRADE_HISTORY/service.py
+++ services/GRADE_HISTORY/service.py
@@ -1,7 +1,22 @@
-from enpoints import GRADE_HISTORY
-from services.GRADE_HISTORY.parser import fetch_grade_history
-from src.session import build_cookies
 import httpx
+import logging
+from enpoints import GRADE_HISTORY
+from src.session import build_cookies
+from services.GRADE_HISTORY.parser import fetch_grade_history
+
+logger=logging.getLogger(__name__)
 async def get_history(client,vtop_engine,data1):
-    r=await client.post(GRADE_HISTORY,data=data1,cookies=build_cookies(vtop_engine))
-    fetch_grade_history(r.text)
+    try:
+        logger.info("Requesting Grade History")
+        r=await client.post(GRADE_HISTORY,data=data1,cookies=build_cookies(vtop_engine))
+        r.raise_for_status()
+        logger.info("Request Success")
+        
+        fetch_grade_history(r.text)
+    except httpx.HTTPError as e:
+        logger.error("Failed to fetch grade history: %s", e)
+        return None
+
+    except Exception:
+        logger.exception("Unexpected error while fetching grade history")
+        return None
